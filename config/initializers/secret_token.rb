0c9548f1a436a41a37310b76218e1d58ba293b57
Max Liu <dr.max.liu@gmail.com>
2013-09-18 08:56:36 -0700

    Initial Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actions::Application.config.secret_token = '4e599a5061206b2ab664b3837b6b7eabebe530713d9e52f72d3aaef3733bf77f61252fa7314a2c9092b7013f09987e2faa9727b877885eee9e5f36862d53515b'
